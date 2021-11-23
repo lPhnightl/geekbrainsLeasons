b25b2d6b5f012e99692d6443f8ca6b35b87423bc
Alldor <lphnightl@gmail.com>
2021-11-23 22:47:22 +0300

    Example 013 changed

--- Example013_RecursionAlgorithm/Program.cs
+++ Example013_RecursionAlgorithm/Program.cs
@@ -94,6 +94,32 @@ void FillImage(int row, int col)
         FillImage(row, col + 1);
     }
 }
-PrintImage(pic);
-FillImage(13,13);
-PrintImage(pic);
+// PrintImage(pic);
+// FillImage(13,13);
+// PrintImage(pic);
+
+
+double Factorial (int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1)+f(n-2)
+
+double Fibonacci(int n)
+{
+    if (n == 1||n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 10; i++)
+{
+    System.Console.WriteLine($"f{i} = {Fibonacci(i)}");
+}
